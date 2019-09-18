87b49b7ac18c0e6418ababf2cc260f68320a85b2
N01272902 <55503132+N01272902@users.noreply.github.com>
2019-09-18 14:22:34 -0400

    Description of what changed !

--- app/src/main/java/com/CENG317/apppract/MainActivity.java
+++ app/src/main/java/com/CENG317/apppract/MainActivity.java
@@ -1,16 +1,34 @@
 package com.CENG317.apppract;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     //This is a demo
-    // I will add some code
+    // I added some code for toast
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button button = findViewById(R.id.button_id);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Context context = getApplicationContext();
+                CharSequence text = "Here's to the toast!";
+                int duration = Toast.LENGTH_LONG;
+
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+            }
+        });
+
+
     }
 }
